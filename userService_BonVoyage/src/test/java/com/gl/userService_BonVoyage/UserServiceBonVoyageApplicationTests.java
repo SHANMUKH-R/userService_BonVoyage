14e0a8b8ddf83d18261a3334835dbae8625c713d
Shanmukh.R <71865643+SHANMUKH-R@users.noreply.github.com>
2024-06-05 23:05:00 +0530

    Add files via upload

---
+++ userService_BonVoyage/src/test/java/com/gl/userService_BonVoyage/UserServiceBonVoyageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.userService_BonVoyage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceBonVoyageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
