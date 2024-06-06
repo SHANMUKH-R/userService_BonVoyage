1644c990f4293a202f347ff2845ac1a8a8e88ede
Shanmukh.R <shanmukhtaman@gmail.com>
2024-06-06 09:23:32 +0530

    Added project files

---
+++ src/main/java/com/gl/userService_BonVoyage/UserServiceBonVoyageApplication.java
@@ -0,0 +1,13 @@
+package com.gl.userService_BonVoyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceBonVoyageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceBonVoyageApplication.class, args);
+	}
+
+}
